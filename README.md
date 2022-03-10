cd91768a4ac180aa1296d3f4bdd7c532d5db0d9f
Hamzh Freijat <hamzhg2016@gmail.com>
2022-03-10 22:00:18 +0300

    Solved

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# quotes
+# Quotes
+
+- In App class you can use the code to convert json file like the one that used . 
+- Just add the path of the file you need to manipulate with . 
+- If you want to test the code just add your absolute path of the test file . 
---
+++ app/src/main/java/com/quotes/Quotes.java
@@ -0,0 +1,17 @@
+package com.quotes;
+
+import java.util.ArrayList;
+
+public class Quotes {
+   String author ;
+   String text ;
+
+
+   @Override
+   public String toString() {
+      return "Quotes{" +
+              "author='" + author + '\'' +
+              ", text='" + text + '\'' +
+              '}';
+   }
+}
